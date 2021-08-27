11867fc6180abf164ab932c755bc791c561a34f5
Mayara Souza <mms8@cin.ufpe.br>
2021-08-27 16:50:59 -0300

    Adiciona projeto

---
+++ StarrySky/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StarrySky
+//
+//  Created by Mayara Mendonça de Souza on 27/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarrySky/StarrySkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarrySkyApp.swift
+//  StarrySky
+//
+//  Created by Mayara Mendonça de Souza on 27/08/21.
+//
+
+import SwiftUI
+
+@main
+struct StarrySkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
